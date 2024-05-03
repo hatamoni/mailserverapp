26a0774fead39063f612f4f85b9984e2b69ea05e
hatamoni <hatim.ibrahim@yahoo.de>
2024-05-03 11:29:18 +0200

    added handleMethodArgumentNotValid to  GlobalExceptionHandler

--- src/main/java/de/hixdev/mailserverapp/exception/GlobalExceptionHandler.java
+++ src/main/java/de/hixdev/mailserverapp/exception/GlobalExceptionHandler.java
@@ -3,17 +3,25 @@ package de.hixdev.mailserverapp.exception;
 import de.hixdev.mailserverapp.constants.Constants;
 import de.hixdev.mailserverapp.dto.ErrorResponseDto;
 import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @ControllerAdvice
-public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler  {
 
   private static final Logger LOG = LoggerFactory.getLogger(GlobalExceptionHandler.class);
 
@@ -35,6 +43,25 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
     return new ResponseEntity<>(errorResponseDTO, HttpStatus.NOT_FOUND);
   }
+
+  @Override
+  protected ResponseEntity<Object> handleMethodArgumentNotValid(
+      MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+    Map<String, String> validationErrors = new HashMap<>();
+
+    LOG.error(Constants.LOG_MESSAGE, "handleMethodArgumentNotValid");
+
+    List<ObjectError> validationErrorList = ex.getBindingResult().getAllErrors();
+
+    validationErrorList.forEach(error -> {
+      String fieldName = ((FieldError) error).getField();
+      String validationErrorMsg = error.getDefaultMessage();
+      validationErrors.put(fieldName, validationErrorMsg);
+    });
+
+    return new ResponseEntity<>(validationErrors, HttpStatus.BAD_REQUEST);
+  }
+
 
   @ExceptionHandler(ResourceNotFoundException.class)
   public ResponseEntity<ErrorResponseDto> handleResourceNotFoundException(
